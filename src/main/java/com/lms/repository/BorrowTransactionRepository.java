eeae37c4dcdfdc67fab3e79d5f7923fb9d1a264d
chinmay76-ui <daschinmaya610@gmail.com>
2025-10-25 11:56:04 +0530

    Delete src directory

--- src/main/java/com/lms/repository/BorrowTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lms.repository;
-
-import com.lms.entity.BorrowTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface BorrowTransactionRepository extends JpaRepository<BorrowTransaction, Long> {
-    List<BorrowTransaction> findByUserId(Long userId);
-}
