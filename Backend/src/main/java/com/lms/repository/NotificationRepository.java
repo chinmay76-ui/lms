c4f6e70f84709645b8b54172c9fd3e5a8cf83abd
rishipani <paniamlan5@gmail.com>
2025-10-27 23:47:25 +0530

    included notification repository file in backend

---
+++ Backend/src/main/java/com/lms/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.lms.repository;
+
+import com.lms.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserId(Long userId);
+}
